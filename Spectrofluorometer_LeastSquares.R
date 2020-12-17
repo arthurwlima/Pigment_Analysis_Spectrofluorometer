287aef85b52dadc903d1d301ef26b4902d7f24d6
Arthur Lima <arthurwlima@gmail.com>
2020-12-17 11:33:25 -0300

    Update Spectrofluorometer_LeastSquares.R

--- Spectrofluorometer_LeastSquares.R
+++ Spectrofluorometer_LeastSquares.R
@@ -1,25 +1,28 @@
-#################################################################################
-####################### Arthur Lima 2020.03.17 ##################################
-# Script R para determinar a concentracao de pigmentos clorofilianos em uma     #
-# amostra ambiental heterogenea. Traducao do script Matlab (Neveux, cedido pelo #
-# Marcio).                                                                      #
-# Metodo de minimos quadrados descrito em Neveux & Lantoine (1993)              # 
-# Deep-Sea Research I (40) 1747; Tenorio et al (2005) Est Cost Shelf Sci 531.   #
-#                                                                               #
-#################################################################################
+######################################################################################
+####################### Arthur Lima 2020.03.17 #######################################
+# Script R para determinar a concentracao de pigmentos clorofilianos em amostras     #
+# ambientais heterogenea. Traducao do script Matlab (Neveux, cedido pelo  Marcio).   # 
+# Metodo de minimos quadrados descrito em Neveux & Lantoine (1993)                   # 
+# Deep-Sea Research I (40) 1747; Tenorio et al (2005) Est Cost Shelf Sci 531.        #
+######################################################################################
 
-require('nnls') 
+# Pacote 'nnls' necessario
+install.packages("nnls")
+require('nnls')
+
+# Ajustar o endereco para a pasta onde estao os arquivos de amostras, branco e resultados
 setwd("/home/arthurw/Documents/scripts/Pigment/170320/")
+
 # Ler o arquivo com os metadados do processamento de extracao de pigmentos, contendo:
-d <- read.table("./../Metadados.Amostras.Pigmentos.csv", header=T, sep='\t')
+d <- read.table("./Metadados.Amostras.Pigmentos.csv", header=T, sep='\t')
 
 # Ler o arquivo com os resultados do spectrofluorimetro para o branco (acetona 90%)
-dBr <- read.table("./../branco1.csv", header=T, sep=',', skip=1)
+dBr <- read.table("./branco1.csv", header=T, sep=',', skip=1)
 
 
 # Ler os resultados de excitacao e emissao de 6 pigmentos de referencia
 # ('Chl a', 'Chl b', 'chl c1+2', 'Phe a', 'Phe b', 'Phe c')
-dRef <- read.table("../CLS700V_6pig_2019.dat", sep='\t')
+dRef <- read.table("./CLS700V_6pig_2019.dat", sep='\t')
 mref <- matrix(dRef$V1, nrow=1581,ncol=7)
 
 
@@ -59,12 +62,13 @@ for (i in 1:length(d$Amostra)){
 		ListaEmissaoAmostra <- c(d[i,4], d[i,7])
 		
 		# Arquivos de espectrofluorimetro organizados em pares de excitacao e emissao
-		# Selecionado apenas os valores de emissao. 
+		# Selecionado apenas os valores de emissao (colunas pares do arquivo de resultado). 
 		# Valores de excitacao devem ser os mesmos entre referencias, Branco e amostra
 		listJ <- seq(2,dim(dd)[2]-1, by=2)
 	
-		# ListaEmissao de acordo com as emissoes lidas nos padroes de pigmentos: 51 valores de excitacao
-		# Amostras foram lidas com 52 excitacoes
+		# Criar uma ListaEmissao das amostras de acordo com as emissoes lidas nos padroes de pigmentos.
+		# Padroes foram lidos com 51 valores de excitacao e amostras foram lidas com 52 excitacoes. 
+		# Considerando apenas os 51 primeiros valores de excitacao das amostras
 		for(j in 1:51){
 		  for (k in listJ){
 			  ListaEmissaoAmostra <- c(ListaEmissaoAmostra, ddCor[j,k])
@@ -91,5 +95,5 @@ for (i in 1:length(d$Amostra)){
 	}
 }
 
-print("Amostras encerradas. Merci Neveux.")
+print("Amostras encerradas. Merci Marcio Neveux.")
 
